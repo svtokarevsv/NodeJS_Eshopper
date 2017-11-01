1a3cf9dac2c4899525c5f057a82dbdb525b981ba
Svyatoslav <svtokarevsv@gmail.com>
2017-11-01 02:50:52 -0400

    2

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+app.set('port', (process.env.PORT || 5000));
 app
 	.use(function (req, res, next) {
 		console.log(`${req.method} request for '${req.url}'`)
 		next();
 	})
-	.use(express.static(__dirname));
-app.listen(3000);
-console.log('http://localhost:3000/');
+	.use(express.static(__dirname))
+	.listen(app.get('port'), function () {
+		console.log('Node app is running on port', app.get('port'));
+	});
 module.exports = app;
